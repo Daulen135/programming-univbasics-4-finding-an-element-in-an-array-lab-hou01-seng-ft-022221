243fd139be96e7c9b8d9e81b18bfce255f93e31e
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-15 23:37:29 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def find_element_index(array,value_to_find)
 count=0
 found_value_index=nil
@@ -9,3 +10,19 @@ count+=1
 end
 found_value_index
 end
+=======
+def find_element_index(array, value_to_find)
+  count=0
+value_to_find_index=nil
+while count<array.length do
+  if array[count]==value_to_find
+    value_to_find_index=count
+count
+end
+count+1
+end
+value_to_find_index
+end
+
+
+>>>>>>> ec5b0ce7dae6ea5d3a9be7703795b2ec1ba4166e
